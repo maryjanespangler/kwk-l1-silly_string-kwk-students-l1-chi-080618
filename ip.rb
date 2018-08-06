8abaded9af919b13c60c0519d473f31c5ae993cd
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-06 20:37:45 +0000

    Done.

---
+++ ip.rb
@@ -0,0 +1,13 @@
+def my_fav_things
+
+fav_animal = "dogs"
+fav_place = "Arizona"
+fav_artist = "Bruno Mars"
+
+puts "I love #{fav_animal}"
+puts "I love #{fav_place}"
+puts "I love #{fav_artist}"
+
+end
+
+my_fav_things
